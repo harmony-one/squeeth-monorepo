8c8c6e84ece66edd6000eadf54fe67982308e83c
0x73696d616f <116109421+0x73696d616f@users.noreply.github.com>
2024-07-30 21:37:17 +0300

    chore: deploy zen-bull-netting to harmony mainnet
    Improve Makefile and deploy scripts.

--- packages/zen-bull-netting/script/HarmonyMainnetScript.s.sol
+++ packages/zen-bull-netting/script/HarmonyMainnetScript.s.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.13;
 
 import { DeployScript } from "./DeployScript.s.sol";
 
-contract HarmonyTestnetDeployScript is DeployScript {
-    address public constant ownerAddress = vm.envAddress("OWNER_ADDRESS");
-    address public constant zenBullAddress = 0x9bAD58c14B0b00bb7A48d3F089Fd58778294ad32;
-    address public constant eulerSimpleLensAddress = 0xDEA7821940D9aE0ee7Edb7Ede8c1F1eaf13068A2;
-    address public constant flashZenAddress = 0xC31690d3220900Db714d8C6339715273F5B63a77;
-    address public constant uniFactoryAddress = 0x14d34078f68d07859CF57B25785B923c443DaE71;
+contract HarmonyMainnetDeployScript is DeployScript {
+    address public ownerAddress = vm.envAddress("OWNER_ADDRESS");
+    address public constant zenBullAddress = 0xbD429107EEba27caA8A0a135Fb3037B5a0b1f007;
+    address public constant eulerSimpleLensAddress = 0x0F0C5b40C4502dD718726709cB05534F99c6220d;
+    address public constant flashZenAddress = 0x135b7d561f1D56935bD55e9c93EABDdCB7b95a08;
+    address public constant uniFactoryAddress = 0x12d21f5d0Ab768c312E19653Bf3f89917866B8e8;
 
     uint256 public constant initialMinEthAmount = 1e18;
     uint256 public constant initialMinZenBullAmount = 1;
